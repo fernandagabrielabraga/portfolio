5b44cfe2be0fb1008e7abd875cd0878478e02e0f
fernanda braga <fernanda.gabriela.braga@gmail.com>
2022-10-19 14:05:58 -0600

    version oct

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,12 @@
 class HomepageController < ApplicationController
   def home
   end
+  def download_pdf
+    send_file(
+      "#{Rails.root}/app/assets/docs/Fernanda_Gabriela_Braga_Resume.pdf",
+      type: "application/pdf",
+      disposition: 'inline', # 'inline' if you want to show PDF in the browser instead of downloading it directly
+      x_sendfile: true
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/", to: "homepage#home", as: "root"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/download_pdf', to: 'homepage#download_pdf', as: 'download_pdf'
 end
